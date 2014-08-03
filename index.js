2e69d523b6eb5c4a0f40f65ebdd573fbfd75f633
Ernie Casilla <ecasilla@icloud.com>
2014-08-03 19:11:46 -0400

    fixing the proc file

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var app = require('express')(),
+    morgan  = require('morgan'),
   port = process.env.PORT || 8080;
 
   app.use(require('compression')());
+  app.use(morgan('combined'))
   app.use(require('serve-static')('public/'));
 
   app.listen(port, function() {
