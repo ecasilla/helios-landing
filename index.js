2a9fad5983d95e1ba0c90d5e388dacfe9cf63b9d
Ernie <ecasilla@icloud.com>
2014-08-05 00:48:16 -0400

    add form submission functionality

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 var express = require('express'),
     morgan  = require('morgan'),
     app = express(),
-  port = process.env.PORT || 8080;
+    bodyParser = require('body-parser'),
+    contact,
+    port = process.env.PORT || 8080;
 
   app.use(require('compression')());
-  app.use(morgan('combined'))
+  app.use(morgan('tiny'))
   app.use(express.static(__dirname + '/public'));
   app.disable('x-powered-by');
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  app.post('/',function(req,res){
+  var name = req.body.name,
+      email = req.body.email,
+      message = req.body.message;
+      contact = {
+        name: name,
+        email: email,
+        message: message
+      }
+      console.log(contact);
+  });
   app.listen(port, function() {
     console.log('Server listening on localhost:'+port);
 });
