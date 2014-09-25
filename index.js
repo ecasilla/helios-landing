711cde190af3baa7ab617139af9e6a3d65e1cdb7
Ernie Casilla <ecasilla@icloud.com>
2014-09-25 03:34:52 -0300

    added env vars to index.js

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ var express = require('express'),
 morgan  = require('morgan'),
 nodemailer = require('nodemailer'),
 bodyParser = require('body-parser'),
+dotenv = require('dotenv'),
 app = express(),
+dotenv.load();
 port = process.env.PORT || 8080;
 
 app.use(require('compression')());
@@ -22,31 +24,35 @@ app.post('/',function(req,res,next){
 console.log(name + ' ' + email + ' ' + message);
 
 
-  smtpTrans = nodemailer.createTransport('SMTP', {
+  smtpTrans = nodemailer.createTransport({
+    debug:true,
     service: 'Gmail',
     auth: {
-      user: "ecasilla01@gmail.com",
-      pass: "rugged21"
+      user: process.env.GMAIL_USERNAME,
+      pass: process.env.GMAIL_PASSWORD
     }
   });
 
   contact = {
-    from: name + ' &lt;' + email + '&gt;',
-    to: 'ecasilla01@gmail.com',
-    subject: 'Helios contact form' + ' ' + name,
-    text: message
+    from: process.env.GMAIL_USERNAME,
+    to:   process.env.GMAIL_USERNAME,
+    subject: 'New Helios Contact' + ' ' + name + ' '+ email,
+    text: 'Email: ' +  email + '\n ' + message
   };
 
   smtpTrans.sendMail(contact, function (error, response) {
     if (error) {
        console.log('Error occured');
        console.log(error.message);
+       console.log(error);
+       return;
     }
     else {
       console.log('Success');
     }
-     smtpTransport.close();
+     smtpTrans.close();
   });
+  res.redirect('/').end();
 });
 
 
