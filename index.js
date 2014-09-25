f3118636778a9db00547bc42e2a5ba62c185c79b
Ernie Casilla <ecasilla@icloud.com>
2014-09-25 02:45:17 -0300

    still working on the contact form

--- index.js
+++ index.js
@@ -1,28 +1,55 @@
 var express = require('express'),
-    morgan  = require('morgan'),
-    app = express(),
-    bodyParser = require('body-parser'),
-    contact,
-    port = process.env.PORT || 8080;
+morgan  = require('morgan'),
+nodemailer = require('nodemailer'),
+bodyParser = require('body-parser'),
+app = express(),
+port = process.env.PORT || 8080;
 
-  app.use(require('compression')());
-  app.use(morgan('tiny'))
-  app.use(express.static(__dirname + '/public'));
-  app.disable('x-powered-by');
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({ extended: true }));
+app.use(require('compression')());
+app.use(morgan('tiny'))
+app.use(express.static(__dirname + '/public'));
+app.disable('x-powered-by');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-  app.post('/',function(req,res){
-  // var name = req.body.name,
-  //     email = req.body.email,
-  //     message = req.body.message;
-  //     contact = {
-  //       name: name,
-  //       email: email,
-  //       message: message
-  //     }
-      console.log(req.body);
+app.post('/',function(req,res,next){
+  var contact, smtpTrans, name, email,message;
+
+  name = req.body.name
+  email = req.body.email
+  message = req.body.message
+
+console.log(name + ' ' + email + ' ' + message);
+
+
+  smtpTrans = nodemailer.createTransport('SMTP', {
+    service: 'Gmail',
+    auth: {
+      user: "ecasilla01@gmail.com",
+      pass: "rugged21"
+    }
   });
-  app.listen(port, function() {
-    console.log('Server listening on localhost:'+port);
+
+  contact = {
+    from: name + ' &lt;' + email + '&gt;',
+    to: 'ecasilla01@gmail.com',
+    subject: 'Helios contact form' + ' ' + name,
+    text: message
+  };
+
+  smtpTrans.sendMail(contact, function (error, response) {
+    if (error) {
+       console.log('Error occured');
+       console.log(error.message);
+    }
+    else {
+      console.log('Success');
+    }
+     smtpTransport.close();
+  });
 });
+
+
+  app.listen(port, function() {
+    console.log('Server listening on localhost: '+ port);
+  });
